e2c3002e7f9d0bd24863e53ed638c344af112b9c
MacMini17 <mcm-mac-17@MacMini17s-Mac-mini.local>
2022-04-14 11:54:29 -0700

    first

---
+++ main.js
@@ -0,0 +1,26 @@
+// let superHeroes = [
+//     ['Superman', 'Batman', 'Spiderman'],
+//         ['Wolverine', 'Storm', 'Cyclops'],
+//         ['Goku', 'Vegeta',  'Gohan']
+//     ];
+
+//     let newHeroes = ['Invisible Woman', 'Thing', 'Human Torch'];
+//     superHeroes.push(newHeroes);
+//     superHeroes[1].reverse();
+//     console.log(superHeroes);
+   // Try this
+    
+   // Add a new newHeroes array to the end of superHeroes
+    
+    
+   // Reverse the order of the superHeroes at index 1
+
+    let friends = [
+    ["Adam", "Bob", "Chuck"],
+    ["Dennis", "Earl", "Frank"]
+];
+
+friends[0].push('Jeff');
+friends[1].push('Holmes');
+
+console.log(friends);
